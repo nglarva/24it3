5a42fd90e5c92c6ef7813f444b5538fde486cab5
ngovannamt36@gmail.com <ngovannamt36@gmail.com>
2025-03-19 18:07:51 +0700

    chia thành 3 file

--- main.c
+++ main.c
@@ -1,9 +1,4 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
-#include <ctype.h>
-void nhapTenKH(char name[100]);
-bool checkDigitInStr(char name[100]);
+#include "functions.h"
 
 int main(void) {
     /*
@@ -17,32 +12,7 @@ int main(void) {
         nhapTenKH(name);
         //printf("Do dai: %d\n", strlen(name));        
         
-    } while (checkDigitInStr(name) == 1);     
+    } while (checkDigitInStr(name)==true);     
     printf("Ten KH duoc nhap vao: %s\n", name);
     return 0;
-}
-//Hàm nhập tên khách hàng
-void nhapTenKH(char name[100]){
-    printf("Nhap ten khach hang: ");
-    //scanf("s", name);
-    fgets(name,99,stdin);
-
-    name[strlen(name)] = '\0';
-    printf("%s\n",name);
-}
-
-// Kiểm tra điều kiện trong chuỗi có chứa số hay không
-//Sử dụng các hàm trong thư viện ctype.h
-bool checkDigitInStr(char name[100]){
-
-    for (unsigned int i = 0; i < strlen(name); i++)
-        {
-           if (isdigit(name[i]))
-           {
-                printf ("co so trong ham. Yeu cau nhap lai \n");
-                return true;
-          }
-           
-        }
-    return false;
 }
