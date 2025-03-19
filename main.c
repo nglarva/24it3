83936c0e0d95b4c4aaf7f665c028257bb05c5d20
ngovannamt36@gmail.com <ngovannamt36@gmail.com>
2025-03-19 16:56:37 +0700

    kiểm tra điều kiện

--- main.c
+++ main.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
+#include <string.h>
+void nhapTenKH(char name[100]);
 
 int main(void) {
     /*
     Yêu cầu 1: Viết hàm để kiểm tra xem tên khách hàng có chứa ký tự số không. Nếu có, yêu cầu khách hàng nhập lại tên.
     */
    //Khai báo biến và kiểu dữ liệu để lưu tên khách hàng
-    char name[100];
-   //Sử dụng scanf nhập tên khách hàng từ bàn phím
-    printf("nhap ten khach hang: ");
-    scanf("%[^\n]99s", &name);
-   //In tên khách hàng ra màn hình
-    printf("%s",name);
+    char name[100]="abc4de";
     // Kiểm tra điều kiện trong chuỗi có chứa số hay không
 
-   
-
-
-
+    
+    printf("%s", name);
     return 0;
+}
+
+void nhapTenKH(char name[100]){
+    printf("Nhap ten khach hang: ");
+    scanf("%[^\n]99s", name);
+
+    name[strlen(name)] = '\0';
+    printf("%s\n",name);
 }
