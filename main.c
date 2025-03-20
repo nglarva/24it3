7b05cff5d803b9198d3b4eb451a5eb27ab5f3e78
ngovannamt36@gmail.com <ngovannamt36@gmail.com>
2025-03-20 18:19:58 +0700

    check year,month, day

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
 #include "functions.h"
 #define NOW 2025
+//function prototype
+bool isLeapYear(int year);
+bool checkMonth(int month);
+
+int month31[7] = {1,3,5,7,8,10,12};
+int month30[4] = {4,6,9,11};
 
 int main(void)
 {
@@ -23,20 +29,64 @@ int main(void)
 
     // Nhập năm từ bàn phím. Kiểm tra điều kiện năm nhuận
     int leapyear;
-    do
-    {
-        printf("Nhap nam hien tai:");
-        scanf("%d", &leapyear);
-    }while (leapyear <= 2000 || leapyear >= NOW);
-    if (leapyear % 4 == 0)
-        {
-            printf("La nam nhuan\n");
-        }
-        else
-        {
-            printf("Nam khong nhuan\n");
-        }
-    
+    int month;
+    int day;
+    bool valid = true;
 
+    do {
+        printf("Nhap nam hien tai: \n");
+        scanf("%d", &leapyear);
+
+        if(leapyear < 2000 || leapyear > NOW){
+            valid = false;
+        }
+        printf("nhap thang: \n");
+        scanf("%d", &month);
+
+        if(month<=0 &&month>12)
+        {
+            valid = false;
+        }
+        printf("nhap ngay: \n");
+        scanf("%d", &day);
+
+        if(day<= 0 || day > 31){
+            valid = false;
+        }
+        if(checkMonth(month) == -1 && day > 31)
+        {
+            printf("Thang %d khong co %d ngày", month, day);
+            valid = false;
+        }
+        else if(checkMonth(month) == 0 && isLeapYear(leapyear) && day > 29){
+            printf("Thang %d khong co %d ngày", month, day);
+            valid = false;
+        }
+        else if (checkMonth(month) == 0 && !isLeapYear(leapyear) && day > 28){
+            printf("Thang %d khong co %d ngày", month, day);
+            valid = false;
+        }
+        
+
+
+    }while(valid == false);
+    
     return 0;
+    
+}
+
+bool isLeapYear(int year){
+    return year % 4 == 0 ? 1 : 0;
+}
+bool checkMonth(int month){
+    for(int i=0;i< sizeof(month30)/sizeof(month30[0]);i++){
+        if(month = month30[i]){
+            return -1;
+        }
+    }
+    if(month == 2){
+        return 0;
+    }
+    return 1;
+
 }
